c994b1acb9c4d3a987f65e193fbe37397a5a176a
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 15:12:07 +0300

    first commit

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let sounds = ["boing", "tweet", "doorbell","plop", "drumroll", "rooster","guitar", "harp"]
+    
+    var player = AVAudioPlayer()
 
+    @IBOutlet weak var soundLabel: UILabel!
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEvent.EventSubtype.motionShake {
+            play()
+        }
+    }
+    
+    func play(){
+        let randomInt = Int.random(in: 0...7)
+        
+        let audioPath = Bundle.main.path(forResource: sounds[randomInt], ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            player.play()
+            
+            soundLabel.text = "playing \(sounds[randomInt])"
+            print("playing \(sounds[randomInt])")
+            
+        } catch  {
+            print("AVPlayer Error")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
